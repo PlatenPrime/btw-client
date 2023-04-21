fe119df3c3d9890122dd1361916263803a880817
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2023-04-21 12:03:13 +0300

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://btw-server.onrender.com/api',
+	baseURL: 'https://btw-server.up.railway.app/api',
 })
 
 instance.interceptors.request.use((config) => {
