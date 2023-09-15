bae959f93b435250d2fafa6dda933ca7758d2f0c
PlatenPrime <proshta@gmail.com>
2023-09-15 21:02:17 +0300

    Update ArtFindPage.jsx

--- src/pages/Art/ArtFindPage.jsx
+++ src/pages/Art/ArtFindPage.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 
+import * as XLSX from 'xlsx';
+
+
+
 import { useNavigate } from 'react-router-dom';
 import useArts from '../../hooks/useFetchArts';
 
@@ -34,6 +38,7 @@ const ArtFindPage = () => {
 
 	useEffect(() => {
 		console.log("Загружены такие артикулы: ", artsDB)
+		console.log(Array.isArray(artsDB))
 	}, [artsDB])
 
 
@@ -68,14 +73,58 @@ const ArtFindPage = () => {
 			console.log(artInput.current.value)
 		}
 
-
-
-
-
 	}
 
 
 	const photo = `https://sharik.ua/images/elements_big/${artItem.artikul}_m1.jpg`;
+
+
+
+
+
+
+
+
+
+	function downloadFile(blob, filename) {
+		const url = window.URL.createObjectURL(blob);
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = filename || 'downloaded_file.xlsx';
+		document.body.appendChild(a);
+		a.click();
+		window.URL.revokeObjectURL(url);
+	}
+
+
+	const handleDownloadExcel = () => {
+		console.log("artsDB:", artsDB); // Добавьте это логирование
+
+
+		const workbook = XLSX.utils.book_new();
+		const ws = XLSX.utils.json_to_sheet(artsDB);
+
+		try {
+			// Попытайтесь создать Blob
+			const blob = XLSX.write(workbook, { bookType: 'blob', type: 'blob' });
+
+			// Проверьте, что Blob создан успешно
+			if (blob) {
+				// Вызов функции для скачивания файла
+				downloadFile(blob, 'artsDB.xlsx');
+			} else {
+				console.error('Не удалось создать Blob для Excel-файла.');
+			}
+		} catch (error) {
+			console.error('Произошла ошибка при создании Excel-файла:', error);
+		}
+	};
+
+
+
+
+
+
 
 	return (
 
@@ -95,53 +144,53 @@ const ArtFindPage = () => {
 
 
 
-	
-
-
-
-		
-					<CardBlock className="bg-sky-400/50" >
-						<h2 className='text-2xl'>Артикулы БТрейд</h2>
-
-						{loadingArtsDB && <p>Загрузка данных...</p>}
-						{artsDB && <p>Сейчас в базе данных БТрейд артикулов: <span className='text-lg bg-sky-500 p-1 rounded' >{artsDB.length} </span>  </p>}
-					</CardBlock>
-
-					<form onSubmit={handlerArtFind}>
-
-						<CardBlock className='flex flex-col md:flex-row justify-center items-center  p-4 '>
-
-							<InputBlock
-								className='p-2 text-2xl text-center  text-white '
-								type="text"
-								ref={artInput}
-								placeholder="ХХХХ-ХХХХ"
-							/>
 
 
 
 
-							<ButtonBlock
 
-								className='search p-3 my-1'
-								type="submit"
-							>
-								Найти артикул
-							</ButtonBlock>
+			<CardBlock className="bg-sky-400/50" >
+				<h2 className='text-2xl'>Артикулы БТрейд</h2>
 
-						</CardBlock>
+				{loadingArtsDB && <p>Загрузка данных...</p>}
+				{artsDB && <p>Сейчас в базе данных БТрейд артикулов: <span className='text-lg bg-sky-500 p-1 rounded' >{artsDB.length} </span>  </p>}
+			</CardBlock>
 
-					</form>
+			<form onSubmit={handlerArtFind}>
+
+				<CardBlock className='flex flex-col md:flex-row justify-center items-center  p-4 '>
+
+					<InputBlock
+						className='p-2 text-2xl text-center  text-white '
+						type="text"
+						ref={artInput}
+						placeholder="ХХХХ-ХХХХ"
+					/>
 
 
-					{artCardDisplay && artItem &&
-						<CardBlock
-							className='cursor-pointer p-5 border border-teal-500 '
-							onClick={() => navigate(`/arts/${artItem._id}`)}
-						>
 
 
-							<RowBlock className='
+					<ButtonBlock
+
+						className='search p-3 my-1'
+						type="submit"
+					>
+						Найти артикул
+					</ButtonBlock>
+
+				</CardBlock>
+
+			</form>
+
+
+			{artCardDisplay && artItem &&
+				<CardBlock
+					className='cursor-pointer p-5 border border-teal-500 '
+					onClick={() => navigate(`/arts/${artItem._id}`)}
+				>
+
+
+					<RowBlock className='
 						
 							m-2 pr-5 rounded 
 							flex flex-col  md:flex-row items-center justify-start 
@@ -152,32 +201,39 @@ const ArtFindPage = () => {
 							'>
 
 
-								<ImageBlock
-									src={photo}
-									alt="Здесь должно быть изображение артикула"
-									width="100px"
-									height="100px"
-									className='rounded 
+						<ImageBlock
+							src={photo}
+							alt="Здесь должно быть изображение артикула"
+							width="100px"
+							height="100px"
+							className='rounded 
 									hover:scale-150 hover:translate-x-3 duration-500
 									hover:shadow-xl hover:shadow-white ' />
 
 
 
-								<TextBlock className='text-xl' >{artItem.nameukr}</TextBlock>
-								{/* <TextBlock className='text-xl' >{artItem.namerus}</TextBlock> */}
+						<TextBlock className='text-xl' >{artItem.nameukr}</TextBlock>
+						{/* <TextBlock className='text-xl' >{artItem.namerus}</TextBlock> */}
 
 
 
-							</RowBlock>
+					</RowBlock>
 
 
 
 
 
-						</CardBlock>
-					}
+				</CardBlock>
+			}
 
 
+
+			<CardBlock>
+
+
+				<button onClick={handleDownloadExcel}>Скачать Excel-файл</button>
+
+			</CardBlock>
 
 
 		</PageBTW >
