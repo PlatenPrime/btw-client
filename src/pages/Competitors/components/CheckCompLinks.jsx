c6280cc17109815cd26ca492c161cacbccc02ae4
PlatenPrime <proshta@gmail.com>
2023-10-11 08:40:03 +0300

    Update CheckCompLinks.jsx

--- src/pages/Competitors/components/CheckCompLinks.jsx
+++ src/pages/Competitors/components/CheckCompLinks.jsx
@@ -4,8 +4,8 @@ import { getArtDataBtrade } from '../../../utils/getArtDataBtrade'
 import { getArtDataSharte } from '../../../utils/getArtDataSharte'
 import { getArtDataAir } from '../../../utils/getArtDataAir'
 import { getArtDataBest } from '../../../utils/getArtDataBest'
-import { ButtonBlock, CardBlock, InputBlock, TextBlock } from '../../../components'
-import ModalInfo from '../../../components/UI/Modal/ModalInfo'
+import { ButtonBlock, CardBlock, InputBlock, TextBlock, ModalWrapper } from '../../../components'
+
 
 import { BsClipboardCheck } from 'react-icons/bs';
 
@@ -112,131 +112,145 @@ export default function CheckCompLinks() {
 				className="confirm-c flex"
 				onClick={() => { setShowModalInfo(true) }}
 			>
-				<BsClipboardCheck 
-				className='text-2xl'
+				<BsClipboardCheck
+					className='text-2xl'
 				/>
 				<TextBlock>Перевірити посилання</TextBlock>
 			</ButtonBlock>
 
 			{showModalInfo && <CardBlock>
-				<ModalInfo
+				<ModalWrapper
 					onCancel={() => { setShowModalInfo(false) }}
 
 				>
-					<CardBlock
-						className="flex flex-col items-center space-y-2  p-4"
-					>
-
-						<CardBlock
-							className="flex w-full justify-between  space-x-2"
-						>
-							<TextBlock>Yumi</TextBlock>
-							<InputBlock
-								onChange={(e) => { setArtikulYumi(e.target.value) }}
-								value={artikulYumi}
-								placeholder="Введи ссылку yumi"
-							/>
-							<ButtonBlock
-								className="search-c"
-								onClick={() => { handleFetch(artikulYumi) }}
-								disabled={!artikulYumi}
-							>
-								Проверить
-							</ButtonBlock>
-							<TextBlock>{price}</TextBlock>
-							<TextBlock>{quant}</TextBlock>
-						</CardBlock>
 
 
-						<CardBlock
-							className="flex w-full justify-between  space-x-2"
-						>
-							<TextBlock>Btrade</TextBlock>
-							<InputBlock
-								onChange={(e) => { setArtikulBtrade(e.target.value) }}
-								value={artikulBtrade}
-								placeholder="Введи артикул btrade"
-							/>
-							<ButtonBlock
-								className="search-c"
-								onClick={() => { handleFetchBtrade(artikulBtrade) }}
-								disabled={!artikulBtrade}
-							>
-								Проверить
-							</ButtonBlock>
-							<TextBlock>{priceBtrade}</TextBlock>
-							<TextBlock>{quantBtrade}</TextBlock>
-						</CardBlock>
+					<table>
+						<tbody>
+							<tr>
+
+								<CardBlock className="flex w-full ">
+									<td><TextBlock>Yumi</TextBlock></td>
+									<td>
+										<InputBlock
+											onChange={(e) => { setArtikulYumi(e.target.value) }}
+											value={artikulYumi}
+											placeholder="Введи ссылку yumi"
+										/>
+									</td>
+									<td>
+										<ButtonBlock
+											className="search-c"
+											onClick={() => { handleFetch(artikulYumi) }}
+											disabled={!artikulYumi}
+										>
+											Проверить
+										</ButtonBlock>
+									</td>
+									<td><TextBlock>{price}</TextBlock></td>
+									<td><TextBlock>{quant}</TextBlock></td>
+								</CardBlock>
+
+							</tr>
+							<tr>
+								<CardBlock className="flex w-full">
+									<td><TextBlock>Btrade</TextBlock></td>
+									<td>
+										<InputBlock
+											onChange={(e) => { setArtikulBtrade(e.target.value) }}
+											value={artikulBtrade}
+											placeholder="Введи артикул btrade"
+										/>
+									</td>
+									<td>
+										<ButtonBlock
+											className="search-c"
+											onClick={() => { handleFetchBtrade(artikulBtrade) }}
+											disabled={!artikulBtrade}
+										>
+											Проверить
+										</ButtonBlock>
+									</td>
+									<td><TextBlock>{priceBtrade}</TextBlock></td>
+									<td><TextBlock>{quantBtrade}</TextBlock></td>
+								</CardBlock>
+							</tr>
+							<tr>
+								<CardBlock className="flex w-full ">
+									<td><TextBlock>Sharte</TextBlock></td>
+									<td>
+										<InputBlock
+											onChange={(e) => { setArtikulSharte(e.target.value) }}
+											value={artikulSharte}
+											placeholder="Введи ссылку sharte"
+										/>
+									</td>
+									<td>
+										<ButtonBlock
+											className="search-c"
+											onClick={() => { handleFetchSharte(artikulSharte) }}
+											disabled={!artikulSharte}
+										>
+											Проверить
+										</ButtonBlock>
+									</td>
+									<td><TextBlock>{priceSharte}</TextBlock></td>
+									<td><TextBlock>{isAvailableSharte ? "Есть" : isAvailableSharte === false ? "Нет" : ""}</TextBlock>
+									</td>
+								</CardBlock>
+							</tr>
+							<tr>
+								<CardBlock className="flex ">
+									<td><TextBlock>Air</TextBlock></td>
+									<td>
+										<InputBlock
+											onChange={(e) => { setArtikulAir(e.target.value) }}
+											value={artikulAir}
+											placeholder="Введи ссылку air"
+										/>
+									</td>
+									<td>
+										<ButtonBlock
+											className="search-c"
+											onClick={() => { handleFetchAir(artikulAir) }}
+											disabled={!artikulAir}
+										>
+											Проверить
+										</ButtonBlock>
+									</td>
+									<td><TextBlock>{priceAir}</TextBlock></td>
+									<td><TextBlock>{isAvailableAir ? "Есть" : isAvailableAir === false ? "Нет" : ""}</TextBlock></td>
+								</CardBlock>
+							</tr>
+							<tr>
+								<CardBlock className="flex ">
+									<td><TextBlock>Best</TextBlock></td>
+									<td>
+										<InputBlock
+											onChange={(e) => { setArtikulBest(e.target.value) }}
+											value={artikulBest}
+											placeholder="Введи ссылку best"
+										/>
+									</td>
+									<td>
+										<ButtonBlock
+											className="search-c"
+											onClick={() => { handleFetchBest(artikulBest) }}
+											disabled={!artikulBest}
+										>
+											Проверить
+										</ButtonBlock>
+									</td>
+									<td><TextBlock>{priceBest}</TextBlock></td>
+									<td><TextBlock>{isAvailableBest ? "Есть" : isAvailableBest === false ? "Нет" : ""}</TextBlock></td>
+								</CardBlock>
+							</tr>
+						</tbody>
+					</table>
 
 
-						<CardBlock
-							className="flex w-full justify-end  space-x-2"
 
-						>
-							<TextBlock>Sharte</TextBlock>
-							<InputBlock
-								onChange={(e) => { setArtikulSharte(e.target.value) }}
-								value={artikulSharte}
-								placeholder="Введи ссылку sharte"
-							/>
-							<ButtonBlock
-								className="search-c"
-								onClick={() => { handleFetchSharte(artikulSharte) }}
-								disabled={!artikulSharte}
-							>
-								Проверить
-							</ButtonBlock>
-							<TextBlock>{priceSharte}</TextBlock>
-							<TextBlock>{isAvailableSharte ? "Есть" : isAvailableSharte === false ? "Нет" : ""}</TextBlock>
-						</CardBlock>
-
-
-						<CardBlock
-							className="flex justify-between  space-x-2"
-						>
-							<TextBlock>Air</TextBlock>
-							<InputBlock
-								onChange={(e) => { setArtikulAir(e.target.value) }}
-								value={artikulAir}
-								placeholder="Введи ссылку air"
-							/>
-							<ButtonBlock
-								className="search-c"
-								onClick={() => { handleFetchAir(artikulAir) }}
-								disabled={!artikulAir}
-							>
-								Проверить
-							</ButtonBlock>
-							<TextBlock>{priceAir}</TextBlock>
-							<TextBlock>{isAvailableAir ? "Есть" : isAvailableAir === false ? "Нет" : ""}</TextBlock>
-						</CardBlock>
-
-
-
-						<CardBlock
-							className="flex justify-between  space-x-2"
-						>
-							<TextBlock>Best</TextBlock>
-							<InputBlock
-								onChange={(e) => { setArtikulBest(e.target.value) }}
-								value={artikulBest}
-								placeholder="Введи ссылку best"
-							/>
-							<ButtonBlock
-								className="search-c"
-								onClick={() => { handleFetchBest(artikulBest) }}
-								disabled={!artikulBest}
-							>
-								Проверить
-							</ButtonBlock>
-							<TextBlock>{priceBest}</TextBlock>
-							<TextBlock>{isAvailableBest ? "Есть" : isAvailableBest === false ? "Нет" : ""}</TextBlock>
-						</CardBlock>
-
-					</CardBlock>
-
-
-				</ModalInfo>
+				</ModalWrapper>
 			</CardBlock>}
 		</CardBlock>
 	)
