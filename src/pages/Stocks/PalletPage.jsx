fb0d1bcea752acf059b30e0dee8c5bb324ff138a
PlatenPrime <proshta@gmail.com>
2023-12-10 14:50:02 +0200

    Update PalletPage.jsx

--- src/pages/Stocks/PalletPage.jsx
+++ src/pages/Stocks/PalletPage.jsx
@@ -238,7 +238,7 @@ export default function PalletPage() {
 
 			const existingPos = posesInStore.find(pos => pos.artikul === newPos.artikul);
 
-			if (existingPos && existingPos.sklad === newPos.sklad && existingPos.date === newPos.date) {
+			if (existingPos && existingPos.sklad === newPos.sklad && existingPos.date === newPos.date && existingPos.com === newPos.com) {
 
 				const updatedData = {
 					quant: +existingPos.quant + +newPos.quant,
@@ -464,7 +464,7 @@ export default function PalletPage() {
 
 
 
-				
+
 
 
 				<ModalCreatePos
