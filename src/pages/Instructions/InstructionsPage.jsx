30098b2a5f390174b137acc72a991ea1883d3006
PlatenPrime <proshta@gmail.com>
2024-03-25 07:46:11 +0200

    Update InstructionsPage.jsx

--- src/pages/Instructions/InstructionsPage.jsx
+++ src/pages/Instructions/InstructionsPage.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { ButtonBlock, ButtonGroup, HeaderBlock, PageBTW } from '../../components'
+import React, { useEffect, useState } from 'react'
+import { ButtonBlock, ButtonGroup, CardBlock, ContainerBlock, HeaderBlock, PageBTW, Spinner, TextBlock } from '../../components'
 import { useNavigate } from 'react-router-dom'
+import useInsStore from './insStore'
 
 export default function InstructionsPage() {
 
 
 
 	const navigate = useNavigate()
+
+
+
+	const { instructions, getAllInstructions } = useInsStore()
+
+
+	const [isInsLoading, setIsInsLoading] = useState(false)
+
+
+
+
+	useEffect(() => {
+
+
+		const fetchIns = async () => {
+			try {
+				setIsInsLoading(true)
+
+
+				const instructions = await getAllInstructions()
+
+				console.log("Інструкції завантажені", instructions);
+
+
+			} catch (error) {
+				console.log(error);
+
+			} finally {
+				setIsInsLoading(false)
+			}
+		}
+
+
+
+		fetchIns()
+
+
+	}, [])
+
+
 
 
 
@@ -23,19 +64,30 @@ export default function InstructionsPage() {
 
 
 
+			{isInsLoading ?
 
-			
 
-
-			<ButtonGroup>
-				<ButtonBlock
-					className="green-b"
-					onClick={() => navigate("/ins/new")}
-
+				<ContainerBlock
+					className="w-full h-full flex justify-start items-center"
 				>
-					Створити інструкцію
-				</ButtonBlock>
-			</ButtonGroup>
+					<Spinner color="rgb(59 130 246)" />
+				</ContainerBlock>
+
+				:
+
+				<>
+
+
+
+					<ButtonGroup>
+						<ButtonBlock
+							className="green-b"
+							onClick={() => navigate("/ins/new")}
+
+						>
+							Створити інструкцію
+						</ButtonBlock>
+					</ButtonGroup>
 
 
 
@@ -46,6 +98,57 @@ export default function InstructionsPage() {
 
 
 
+
+					<ContainerBlock>
+						{instructions.length < 1
+							?
+							<TextBlock className="text-blue-100 italic" >Інструкцій немає або вони не завантажились</TextBlock>
+							:
+
+							<CardBlock
+								className="space-y-2"
+							>
+
+								{instructions?.map((ins, i) => <CardBlock
+									className="p-8 cursor-pointer
+									bg-blue-500/10 hover:bg-blue-500 hover:shadow-lg hover:shadow-blue-500 
+									
+									transition ease-in-out duration-1000 
+									
+									
+									"
+									onClick={() => navigate(`/ins/${ins?._id}`)}
+								>
+									<TextBlock
+										className="justify-start"
+									>
+										{i + 1}. {ins?.title}
+										
+									</TextBlock>
+									<CardBlock>{ins?.category}</CardBlock>
+										<CardBlock>{ins?.department}</CardBlock>
+								</CardBlock>
+								)}
+
+
+							</CardBlock>}
+
+					</ContainerBlock>
+
+
+
+
+
+
+
+
+
+
+
+
+
+				</>
+			}
 
 
 		</PageBTW >
