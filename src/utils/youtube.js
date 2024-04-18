d36226c6bec4139aa03d7889c2e81c7f7631deb0
Aleksandr Marchenko <proshta@gmail.com>
2024-04-18 13:39:03 +0300

    Update youtube.js

--- src/utils/youtube.js
+++ src/utils/youtube.js
@@ -1,4 +1,5 @@
 export const extractVideoId = (url) => {
+	if (!url) return null; // Проверка на наличие значения url
 	const regExp = /(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([\w-]{11})/;
 	const match = url.match(regExp);
 	return match && match[1];
