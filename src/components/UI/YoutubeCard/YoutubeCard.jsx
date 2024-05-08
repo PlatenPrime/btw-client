8b693607b7ffcd56e29804110c5419292f22907e
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-05-08 07:10:31 +0300

    08.05.2024

--- src/components/UI/YoutubeCard/YoutubeCard.jsx
+++ src/components/UI/YoutubeCard/YoutubeCard.jsx
@@ -1,28 +1,40 @@
 import React from 'react'
-import YouTube from 'react-youtube';
-import { extractVideoId } from '../../../utils/youtube';
-import CardBlock from '../blocks/CardBlock';
+
+	;
+
+import { FaPlay } from "react-icons/fa";
+
+import { TextBlock,  ContainerBlock } from '../../index'
+import ReactPlayer from 'react-player/youtube'
 
 export default function YoutubeCard({
 	url
 }) {
 
-	const id = extractVideoId(url)
+
 
 	return (
 
-		<CardBlock
-			className="flex justify-center items-center bg-blue-500/10 p-2 rounded-xl  w-full"
+
+		<ContainerBlock
+			className="shadow-lg  w-full flex justify-center items-center"
 		>
-			<CardBlock
-				className="shadow-lg shadow-blue-500 "
-			>
-				<YouTube
-					videoId={id}
-					
-				/>
-			</CardBlock>
-		</CardBlock>
+			<ReactPlayer
+				url={url}
+				light
+				controls
+				playIcon={
+					<TextBlock
+						className="text-5xl text-sky-500 hover:text-sky-400"
+					>
+						<FaPlay />
+					</TextBlock>
+				}
+
+
+			/>
+		</ContainerBlock>
+
 
 	)
 }
