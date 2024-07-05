2f440ef8a5934ebd2dd15bb943e09e0c38855362
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-07-05 08:23:31 +0300

    05.07.2024

--- src/pages/Instructions/components/InsBage.jsx
+++ src/pages/Instructions/components/InsBage.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { FcFolder } from 'react-icons/fc'
 import { LuFileEdit, LuFilePlus } from "react-icons/lu";
 import { GoPerson } from "react-icons/go";
+import { MdOutlineOndemandVideo } from 'react-icons/md';
 
 export default function InsBage({
     ins,
@@ -20,17 +21,19 @@ export default function InsBage({
     return (
         <CardBlock
             key={ins?._id}
-            className=" grid grid-cols-1 lg:grid-cols-5 gap-2 w-full p-2 rounded-xl cursor-pointer
+            className=" grid grid-cols-1 
+            lg:grid-cols-[minmax(400px,_1fr)_minmax(200px,_1fr)_minmax(200px,_1fr)_1fr]
+            gap-2 w-full p-2 rounded-xl cursor-pointer
             bg-gradient-to-b from-blue-500/80 to-blue-700/50 hover:bg-blue-500 
             hover:shadow-lg hover:shadow-blue-500
      transition duration-500 ease-in-out"
-            onClick={() => navigate(`/ins/${ins._id}`)}
+            onClick={() => window.open(`/ins/${ins._id}`, "_blank")}
         >
 
 
 
             <CardBlock
-                className="grid grid-cols-1 lg:col-span-2  lg:grid-cols-2 gap-2"
+                className="grid grid-cols-1 lg:col-span-2  lg:grid-cols-[200px_minmax(200px,_1fr)] gap-2"
             >
 
                 <CardBlock
@@ -51,26 +54,29 @@ export default function InsBage({
 
                 </CardBlock>
 
-                <TextBlock className="text-2xl font-bold lg:justify-start  "> {ins?.title}</TextBlock>
+                <TextBlock className="text-xl font-bold lg:justify-start text-left  "> {ins?.title}  </TextBlock>
 
             </CardBlock>
 
 
 
+            <CardBlock
+                className="flex flex-col justify-center text-sm"
+            >
+                {ins?.videoUrl && <TextBlock ><MdOutlineOndemandVideo size={24} /></TextBlock>}
+                <TextBlock className="text-lg text-blue-100 "> <FcFolder className="text-xl" /> {insFolder?.title}</TextBlock>
+            </CardBlock>
 
-            <TextBlock className="text-lg text-blue-100 "> <FcFolder className="text-xl" /> {insFolder?.title}</TextBlock>
 
 
-
-            <TextBlock className="text-lg  text-slate-200 italic">
-                <GoPerson />  {users?.find((user) => user?._id === ins?.author)?.fullname}
-            </TextBlock>
 
 
             <CardBlock
-                className="flex flex-col justify-center text-base"
+                className="flex flex-col justify-center text-sm"
             >
-
+                <TextBlock className="text-base  text-slate-200 italic">
+                    <GoPerson />  {users?.find((user) => user?._id === ins?.author)?.fullname}
+                </TextBlock>
 
                 {ins?.createdAt && <TextBlock className=" text-slate-200 ">
                     <span><LuFilePlus /></span>
