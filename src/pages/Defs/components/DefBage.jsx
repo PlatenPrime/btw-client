af7a2e3f04f5648514d0e79b19bf0a2a57d62a0b
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-08-10 14:40:22 +0300

    10.08.2024

--- src/pages/Defs/components/DefBage.jsx
+++ src/pages/Defs/components/DefBage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ButtonBlock, CardBlock, ImageArt, TextBlock } from '../../../components';
 import { AddIcon } from '../../../components/UI/Icons';
 import ArtBlock from '../../../components/UI/ArtBlock/ArtBlock';
+import { GoDiff, GoGlobe, GoNote } from 'react-icons/go';
 
 
 
@@ -13,34 +14,67 @@ export default function DefBage({
 }) {
 
 
+
+
+    const status = asks?.find(ask => ask.artikul === def.artikul)?.status
+
+
     return (
         <CardBlock
-            className="grid text-sm  lg:grid-cols-6 gap-2  bg-gradient-to-b from-slate-700 to-slate-900   rounded-xl
+            className="grid text-sm  lg:grid-cols-6 gap-2 bg-gradient-to-b from-slate-700 to-slate-900   rounded-xl shadow-2-strong shadow-white/50 overflow-hidden
             hover:shadow-md hover:shadow-white/50
             transition ease-in-out duration-500
             
             "
         >
-          
 
-            <ArtBlock artikul={def?.artikul} />
 
+            <ArtBlock
+                artikul={def?.artikul}
+               
+            />
+
+
+
+
+            <CardBlock
+                className={` 
+                    lg:col-span-1 flex justify-center items-center `}
+            >
+
+                {status === "new" ?
+                    <TextBlock className=" text-center text-white p-2 bg-gradient-to-b from-indigo-500 to-indigo-800 rounded-xl">
+                        Запит в роботі
+                    </TextBlock>
+                    :
+                    <ButtonBlock
+                        className="indigo-b"
+                        onClick={() => {
+                            setShowModalCreateAsk(true)
+                            setNewAskArtikul(def.artikul)
+                        }}
+                        disabled={status === "new"}
+                    >
+                        <AddIcon />
+                        Запит
+                    </ButtonBlock>
+                }
+
+            </CardBlock>
 
 
             <CardBlock
                 className="lg:col-span-2 justify-self-stretch flex flex-col items-start justify-around  rounded-lg "
             >
-
-
                 <CardBlock
                     className=" p-2 flex justify-between w-full "
                 >
                     <TextBlock className=" " >
-                        Запаси:
+                        <GoNote  color='rgb(110 231 183)' />   Запаси:
                     </TextBlock>
 
                     <TextBlock
-                        className=""
+                        className="text-emerald-300"
                     >
                         {def?.stockQuant}
                     </TextBlock>
@@ -51,11 +85,11 @@ export default function DefBage({
                     className=" p-2 flex justify-between w-full "
                 >
                     <TextBlock className=" " >
-                        База:
+                    <GoGlobe color='rgb(147 197 253)' />  База:
                     </TextBlock>
 
                     <TextBlock
-                        className=""
+                        className="text-blue-300"
                     >
                         {def?.btradeQuant}
                     </TextBlock>
@@ -66,6 +100,7 @@ export default function DefBage({
                     className=" p-2 flex justify-between w-full  "
                 >
                     <TextBlock className=" " >
+                        <GoDiff color='rgb(239 68 68)' />
                         Дефіцит:
                     </TextBlock>
 
@@ -73,38 +108,7 @@ export default function DefBage({
                 </CardBlock>
             </CardBlock>
 
-            <CardBlock
-                className={`
-                    
-                    ${asks?.find(ask => ask.artikul === def.artikul)?.status === "new" ?
-                        "bg-gradient-to-b from-indigo-500 to-indigo-800"
-                        :
-                        ""}
-                    
-                    lg:col-span-1 flex justify-center items-center rounded-xl`}
-            >
 
-                {asks?.find(ask => ask.artikul === def.artikul)?.status === "new" ?
-                    <TextBlock className=" text-center text-white p-2">
-                        Запит в роботі
-                    </TextBlock>
-                    :
-                    <ButtonBlock
-                        className="indigo-b"
-                        onClick={() => {
-                            setShowModalCreateAsk(true)
-                            setNewAskArtikul(def.artikul)
-                        }}
-                        disabled={asks?.find(ask => ask.artikul === def.artikul)?.status === "new"}
-                    >
-                        <AddIcon />
-                        Створити запит
-                    </ButtonBlock>
-                }
-
-
-
-            </CardBlock>
 
 
 
